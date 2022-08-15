501b94a192046244bae160f7893da98f24c0785b
Viviana <vivianacg8@gmail.com>
2022-08-16 01:43:52 +0900

    set up search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Viviana Castellanos</footer>
+      </div>
     </div>
   );
 }
